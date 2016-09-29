1bee164a7348c549e63d35c02ae3cf4e3d466f00
Adam Gincel <agincel@stevens.edu>
2016-09-29 19:20:48 -0400

    fixed mobile layout

--- data/page.js
+++ data/page.js
@@ -25,7 +25,7 @@ function getPageContents($http)
 	}
 	return {
 		"data": [
-			{"title": "Adam Gincel's Repository", "content": ["My name is Adam Gincel; I'm a Computer Science undergraduate student attending Stevens Institute of Technology. I program, write music, and make games. This website is a repository for some of my work. Thank you for visiting!"]},
+			{"title": "Repository", "content": ["My name is Adam Gincel; I'm a Computer Science undergraduate student attending Stevens Institute of Technology. I program, write music, and make games. This website is a repository for some of my work. Thank you for visiting!"]},
 			{"title": "Games Showcase", "content": ["Throughout my time enrolled at Stevens I have been very active in the Stevens Game Development Club. I have learned multiple game engines and have made several games on my own and for club Game Jams. Here is a list of games I have been involved in."]},
 			{"title": "Music", "content": ["My music making handle is WaveParadigm, and I post most of the music I make to http://waveparadigm.bandcamp.com. I'll also be embedding some of my albums here on this page. Enjoy!"]},
 			{"title": "Programming Projects", "content": ["This page is home to any side projects I develop. School projects most likely won't end up here, unless there's something special about them."]},
